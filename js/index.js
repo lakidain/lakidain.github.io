5b27b317ad4ebc0fdfb9b388defdb03da670deb9
Ander Lakidain de Arriba <lakidainander@gmail.com>
2022-01-30 03:00:39 +0100

    Updated dark mode and description
    Updated webpage with a new dark mode more intuitive.
    Updated about me text with new position!

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 /*==================== RANDOM IMAGE ========================*/
-
+$(document).ready(function() {
 var astronaut_number = Math.floor(Math.random() * 6)
 
 if(astronaut_number > 5){
@@ -18,22 +18,22 @@ const iconTheme = "fa-sun";
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+const checkbox = localStorage.getItem("checkbox");
+
+console.log(checkbox)
+document.querySelector('#theme-button').checked = (checkbox === 'true')?true:false
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () =>
     document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-    themeButton.classList.contains(iconTheme) ? "fa-moon" : "fa-sun";
+    
+
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
     document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
         darkTheme
-    );
-    themeButton.classList[selectedIcon === "fa-moon" ? "add" : "remove"](
-        iconTheme
     );
 }
 
@@ -44,5 +44,6 @@ themeButton.addEventListener("click", () => {
     themeButton.classList.toggle(iconTheme);
     // We save the theme and the current icon that the user chose
     localStorage.setItem("selected-theme", getCurrentTheme());
-    localStorage.setItem("selected-icon", getCurrentIcon());
+    localStorage.setItem("checkbox", document.querySelector('#theme-button').checked);
+});
 });
